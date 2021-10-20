d1813dfe648879023e078093bb70bfaa1b751d67
JuggBar <juggbar12@gmail.com>
2021-10-20 17:01:37 -0400

    asd

---
+++ FILESTREAMER_WPF/FILESTREAMER_WPF/Contenido.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+namespace FILESTREAMER_WPF
+{
+    /// <summary>
+    /// Interaction logic for Contenido.xaml
+    /// </summary>
+    public partial class Contenido : Window
+    {
+        string File;
+        byte l;
+        FileStream d;
+        string r;
+        public Contenido(string file, byte a)
+        {
+            InitializeComponent();
+            tbContenido.LostFocus += (sender, e)=>{
+                if (string.IsNullOrWhiteSpace(tbContenido.Text))
+                {
+                    tbContenido.Text = "Ingrese contenido aqui";
+                }
+            };
+            tbContenido.GotFocus += (sender, e) =>
+            {
+                if(tbContenido.Text == "Ingrese contenido aqui")
+                {
+                    tbContenido.Text = "";
+                }
+            };
+            this.KeyDown += (sender, e) =>
+            {
+                if(e.Key == Key.Enter)
+                {
+                    r += "\n";
+                }
+            };
+            File = file;
+            if (a == 0)
+            { btn.Visibility = Visibility.Visible; btnDel.Visibility = Visibility.Hidden; }
+            else
+            {
+                d = new FileStream(file, FileMode.Open, FileAccess.Read);
+                tbContenido.Text = new StreamReader(d).ReadToEnd();
+                btn.Visibility = Visibility.Hidden;
+                btnDel.Visibility = Visibility.Visible;
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            d = new FileStream(File, FileMode.Append);
+            d.Write(Encoding.ASCII.GetBytes(r), 0, Encoding.ASCII.GetBytes(r).Length);
+            d.Close();
+            this.Close();
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            d.Close();
+            MainWindow.del(File);
+            this.Close();
+        }
+
+        private void TbContenido_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            r += tbContenido.Text[tbContenido.Text.Length-1];
+        }
+    }
+}
---
+++ FILESTREAMER_WPF/FILESTREAMER_WPF/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.IO;
+namespace FILESTREAMER_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer k = new DispatcherTimer(DispatcherPriority.Normal);
+        static FileStream d;
+        string[] g;
+        string archivo;
+        Contenido f;
+        public MainWindow()
+        {
+            InitializeComponent();
+            k.Interval = new TimeSpan(0, 0, 1);
+            k.Tick += (a, e) =>
+            {
+                fill();
+            };
+            k.Start();
+        }
+        string file_name;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            file_name = tbfile_name.Text;
+            archivo = $@"ARCHIVO\{file_name}.txt";
+            if (g.Contains(archivo))
+            {
+                MessageBoxResult resultado = MessageBox.Show("El archivo ya exite desea sobreescribirlo?", "Error", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (resultado == MessageBoxResult.Yes)
+                {
+                    file_stream(archivo, FileMode.Truncate);
+                    d.Close();
+                    instaciaContenido(archivo,0);
+                }
+            }
+            else
+            {
+                file_stream(archivo, FileMode.Create);
+                fill();
+                instaciaContenido(archivo,0);
+            }
+            
+            
+        }
+        void file_stream(string ar, FileMode fileMode)
+        {
+
+            d = new FileStream(ar, fileMode);
+            if (fileMode == FileMode.Truncate)
+                d.SetLength(0);
+            d.Close();
+            
+        }
+        private void CbFiles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                instaciaContenido(g[cbFiles.SelectedIndex], 1);
+            }
+            catch
+            {
+                cbFiles.SelectedIndex = -1;
+            }
+        }
+        void fill()
+        {
+            g = Directory.GetFiles("ARCHIVO");
+            cbFiles.ItemsSource = g;
+        }
+        void instaciaContenido(string arc, byte a)
+        {
+            f = new Contenido(arc, a);
+            f.ShowDialog();
+        }
+        public static void del(string ar)
+        {
+            File.Delete(ar);
+        }
+    }
+}
